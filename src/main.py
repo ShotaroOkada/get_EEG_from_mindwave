f77ef999484d48558bd35117c1d9aaa977bf2990
ShotaroOkada <b1016117@gmail.com>
2020-11-15 17:01:12 +0900

    [feat]write row data to csv

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import thinkgear
 from setting import MINDWAVE_PORT
+import csv
+from datetime import datetime, time
 
 PORT = MINDWAVE_PORT
-for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
-    for p in packets:
-        if isinstance(p, thinkgear.ThinkGearRawWaveData):
-            continue
-        print p
+
+with open('data/new.csv', 'w') as csvfile:
+    w = csv.writer(csvfile, lineterminator='\n')
+
+    for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
+        for p in packets:
+            if isinstance(p, thinkgear.ThinkGearRawWaveData):
+                print p
+                now = datetime.now().time()
+                w.writerow([now.isoformat(), p])
+            else:
+                continue
